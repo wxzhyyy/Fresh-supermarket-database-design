d59ef1adb6f2c2742888f318fa6a0d2b522d0c87
柴巧霞 <31801062@stu.zucc.edu.cn>
2020-07-05 18:14:08 +0800

    Update DBUtil.java

--- FreshNetMarket/src/fm/util/DBUtil.java
+++ FreshNetMarket/src/fm/util/DBUtil.java
@@ -1,9 +1,11 @@
 package fm.util;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+
 
 public class DBUtil {
-	private static final String jdbcUrl="jdbc:mysql://localhost:3306/personplan?useUnicode=true&characterEncoding=UTF-8";
+	private static final String jdbcUrl="jdbc:mysql://localhost:3306/fresh?useUnicode=true&characterEncoding=UTF-8";
 	private static final String dbUser="root";
 	private static final String dbPwd="123456";
 	static{
@@ -16,5 +18,24 @@ public class DBUtil {
 	}
 	public static Connection getConnection() throws java.sql.SQLException{
 		return java.sql.DriverManager.getConnection(jdbcUrl, dbUser, dbPwd);
+	}
+	public static void main(String args[]) throws DbException {
+		Connection conn=null;
+		try {
+			conn=DBUtil.getConnection();
+			System.out.print(conn);
+		}  catch (SQLException e) {
+			e.printStackTrace();
+			throw new DbException(e);
+		}
+		finally{
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		}
 	}
 }
