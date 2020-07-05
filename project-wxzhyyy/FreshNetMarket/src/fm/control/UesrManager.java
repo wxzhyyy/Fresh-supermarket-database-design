ba9b93951b50de3217d25a8579ec8544735a0819
柴巧霞 <31801062@stu.zucc.edu.cn>
2020-07-05 18:55:59 +0800

    Create UesrManager.java

---
+++ project-wxzhyyy/FreshNetMarket/src/fm/control/UesrManager.java
@@ -0,0 +1,29 @@
+package fm.control;
+
+import fm.itf.IUesrManager;
+import fm.model.BeanUser;
+import fm.util.BaseException;
+
+public class UesrManager implements IUesrManager {
+
+	@Override
+	public BeanUser reg(BeanUser user) throws BaseException {
+		// TODO Auto-generated method stub
+		
+		return null;
+	}
+
+	@Override
+	public BeanUser login(String userid, String pwd) throws BaseException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void changePwd(BeanUser user, String oldPwd, String newPwd, String newPwd2) throws BaseException {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
