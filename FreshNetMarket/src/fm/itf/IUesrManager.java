528a6f4b85166c3356e0f08339aaa68aa08b1fc8
柴巧霞 <31801062@stu.zucc.edu.cn>
2020-07-05 13:09:05 +0800

    Create IUesrManager.java

---
+++ FreshNetMarket/src/fm/itf/IUesrManager.java
@@ -0,0 +1,39 @@
+package fm.itf;
+
+import fm.model.BeanUser;
+import fm.util.BaseException;
+
+public interface IUesrManager {
+	/**
+	 * 注册：
+	 * 要求用户名不能重复，不能为空
+	 * 两次输入的密码必须一致，密码不能为空
+	 * 如果注册失败，则抛出异常
+	 * @param userid
+	 * @param pwd  密码
+	 * @param pwd2 重复输入的密码
+	 * @return
+	 * @throws BaseException
+	 */
+	public BeanUser reg(String userid, String pwd,String pwd2) throws BaseException;
+	/**
+	 * 登陆
+	 * 1、如果用户不存在或者密码错误，抛出一个异常
+	 * 2、如果认证成功，则返回当前用户信息
+	 * @param userid
+	 * @param pwd
+	 * @return
+	 * @throws BaseException
+	 */
+	public BeanUser login(String userid,String pwd)throws BaseException;
+	/**
+	 * 修改密码
+	 * 如果没有成功修改，则抛出异常
+	 * @param user    当前用户
+	 * @param oldPwd  原密码
+	 * @param newPwd  新密码
+	 * @param newPwd2 重复输入的新密码
+	 */
+	public void changePwd(BeanUser user, String oldPwd,String newPwd, String newPwd2)throws BaseException;
+
+}
