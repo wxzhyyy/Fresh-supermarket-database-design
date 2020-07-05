e37ac87324f2fd724a8373ff9953b49dc325549c
柴巧霞 <31801062@stu.zucc.edu.cn>
2020-07-05 18:55:57 +0800

    Create BaseException.java

---
+++ project-wxzhyyy/FreshNetMarket/src/fm/util/BaseException.java
@@ -0,0 +1,12 @@
+package fm.util;
+
+public class BaseException  extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public BaseException(String msg){
+		super(msg);
+	}
+}
